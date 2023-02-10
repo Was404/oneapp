698b20a94d7476cc2b9bf4f2dfad074d5e80b27e
Was404 <vladimirovichdanil@bk.ru>
2023-02-10 12:04:22 +0300

    New WebActivity

--- app/src/main/java/win/winwin/oneapp/WebActivity.java
+++ app/src/main/java/win/winwin/oneapp/WebActivity.java
@@ -53,19 +53,4 @@ public class WebActivity extends AppCompatActivity {
         webSettings.setAllowContentAccess(true);
         //КОНЕЦ ВЕБВЬЮ
     }
-    public class WebViewClient extends android.webkit.WebViewClient {
-        @TargetApi(Build.VERSION_CODES.N)
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-            view.loadUrl(request.getUrl().toString());
-            return true;
-        }
-
-        //для старых устройств
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
-    }
 }
